6c20f699caeda5434fb8fe80379b045c5e7b5429
Ivan Bojer <ijb@networkrift.com>
2018-10-29 10:49:29 -0700

    updated copyright

--- smartapps/ivanbojer/google-sheets-logging.src/google-sheets-logging.groovy
+++ smartapps/ivanbojer/google-sheets-logging.src/google-sheets-logging.groovy
@@ -1,7 +1,7 @@
 /*
  * SmartThings example Code for Google sheets logging
  *
- * Copyright 2016 Charles Schwer
+ * Copyright 2018 Ivan Bojer (original by Charles Schwer)
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at:
